a02e0774cd6db664fb4c461a62ee825e9488be8d
Kittidetchogun <141381533+Kittidetchogun@users.noreply.github.com>
2024-09-14 22:45:06 +0700

    Update todo.js

--- cell03/ex04/ex03bis/todo.js
+++ cell03/ex04/ex03bis/todo.js
@@ -1,30 +1,31 @@
-$(document).ready(function(){
-    const ftList = document.getElementById('ft_list');
-
+$(document).ready(function() {
+    // Function to add a new TO DO item
     function addTodo(text) {
-        const todoDiv = document.createElement('div');
-        todoDiv.classList.add('todo');
-        todoDiv.textContent = text;
+        const $todoDiv = $('<div class="todo"></div>').text(text);
 
-        todoDiv.addEventListener('click', () => {
+        // Add click event to remove the TO DO
+        $todoDiv.click(function() {
             if (confirm('Do you want to remove this TO DO?')) {
-                ftList.removeChild(todoDiv);
-                saveTodos();
+                $(this).remove();
+                saveTodos(); // Save list after removal
             }
         });
 
-        ftList.insertBefore(todoDiv, ftList.firstChild);
-        saveTodos();
+        // Insert new TO DO at the top of the list
+        $('#ft_list').prepend($todoDiv);
+        saveTodos(); // Save list after addition
     }
 
+    // Function to save TO DOs to cookies
     function saveTodos() {
         const todos = [];
-        document.querySelectorAll('.todo').forEach(todo => {
-            todos.push(todo.textContent);
+        $('.todo').each(function() {
+            todos.push($(this).text());
         });
         document.cookie = `todos=${JSON.stringify(todos)};path=/;expires=Fri, 31 Dec 9999 23:59:59 GMT`;
     }
 
+    // Function to load TO DOs from cookies
     function loadTodos() {
         const cookies = document.cookie.split(';');
         let todosCookie = cookies.find(cookie => cookie.trim().startsWith('todos='));
@@ -36,15 +37,17 @@ $(document).ready(function(){
             });
         }
     }
-    $("#newButton").click(newTodo);
-    function newTodo(){
+
+    // Event listener for 'New' button to create a new TO DO
+    $('#newButton').click(function() {
         const todoText = prompt('Enter a new TO DO:');
         if (todoText && todoText.trim() !== '') {
-            addTodo(todoText);
+            addTodo(todoText); // Add TO DO if it's not empty
         } else {
             alert('The TO DO cannot be empty.');
         }
-    }
+    });
 
-    $(window).on("load", loadTodos);
+    // Load TO DOs when the page is loaded
+    loadTodos();
 });
