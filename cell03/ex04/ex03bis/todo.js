227c615bf91289ea000405426b4665d1d6a92307
Kittidetchogun <141381533+Kittidetchogun@users.noreply.github.com>
2024-09-14 13:48:04 +0700

    cell3.4

---
+++ cell03/ex04/ex03bis/todo.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+    const ftList = document.getElementById('ft_list');
+
+    function addTodo(text) {
+        const todoDiv = document.createElement('div');
+        todoDiv.classList.add('todo');
+        todoDiv.textContent = text;
+
+        todoDiv.addEventListener('click', () => {
+            if (confirm('Do you want to remove this TO DO?')) {
+                ftList.removeChild(todoDiv);
+                saveTodos();
+            }
+        });
+
+        ftList.insertBefore(todoDiv, ftList.firstChild);
+        saveTodos();
+    }
+
+    function saveTodos() {
+        const todos = [];
+        document.querySelectorAll('.todo').forEach(todo => {
+            todos.push(todo.textContent);
+        });
+        document.cookie = `todos=${JSON.stringify(todos)};path=/;expires=Fri, 31 Dec 9999 23:59:59 GMT`;
+    }
+
+    function loadTodos() {
+        const cookies = document.cookie.split(';');
+        let todosCookie = cookies.find(cookie => cookie.trim().startsWith('todos='));
+        if (todosCookie) {
+            todosCookie = todosCookie.split('=')[1];
+            const todos = JSON.parse(todosCookie);
+            todos.forEach(todo => {
+                addTodo(todo);
+            });
+        }
+    }
+    $("#newButton").click(newTodo);
+    function newTodo(){
+        const todoText = prompt('Enter a new TO DO:');
+        if (todoText && todoText.trim() !== '') {
+            addTodo(todoText);
+        } else {
+            alert('The TO DO cannot be empty.');
+        }
+    }
+
+    $(window).on("load", loadTodos);
+});
